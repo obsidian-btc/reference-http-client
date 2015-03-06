71e2f45cdec95e8e32de16a63a09da70ecf2d048
Justin Litchfield <litch@me.com>
2015-03-06 11:07:33 -0500

    Reference passes in an argument to the orders query

--- main.rb
+++ main.rb
@@ -3,14 +3,14 @@ require 'openssl'
 require 'base64'
 
 access_key = '1uujt/6iFcNdgCKPbz8nkQ=='
-secret = 'iTpnsfRp7TWJuBwUkVKPQw=='
+secret     = 'iTpnsfRp7TWJuBwUkVKPQw=='
 
 timestamp = Time.now.to_i
 
 host = 'localhost'
 ssl = false
 request_string = "http://0.0.0.0:8081"
-request = '/orders'
+request = '/orders?state=filled'
 port = 8081
 body = ''
 
@@ -22,7 +22,7 @@ p signature = OpenSSL::HMAC.hexdigest(digest, secret, encoded_request)
 
 client = Vertx::HttpClient.new
 client.host = host
-client.ssl = ssl
+client.ssl  = ssl
 client.port = port
 
 request = client.get(request) do |resp|
