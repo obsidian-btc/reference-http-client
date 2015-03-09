556abfdb2e5bafb33be5e285dd8d74107261c253
Justin Litchfield <litch@me.com>
2015-03-09 09:31:30 -0400

    Make balances and orders requests

--- main.rb
+++ main.rb
@@ -10,41 +10,40 @@ timestamp = Time.now.to_i
 host = 'localhost'
 ssl = false
 request_string = "http://0.0.0.0:8081"
-request = '/orders?state=filled'
+request = '/balances'
 port = 8081
 body = ''
 
-p composed_request = "#{timestamp}#{request_string}#{request}#{body}"
-encoded_request = Base64.encode64(composed_request).chomp
+%w{/balances /orders?state=matching}.each do |request|
+  p composed_request = "#{timestamp}#{request_string}#{request}#{body}"
+  encoded_request = Base64.encode64(composed_request).chomp
 
-digest = OpenSSL::Digest::Digest.new('sha256')
-p signature = OpenSSL::HMAC.hexdigest(digest, secret, encoded_request)
+  digest = OpenSSL::Digest::Digest.new('sha256')
+  p signature = OpenSSL::HMAC.hexdigest(digest, secret, encoded_request)
 
-client = Vertx::HttpClient.new
-client.host = host
-client.ssl  = ssl
-client.port = port
+  client = Vertx::HttpClient.new
+  client.host = host
+  client.ssl  = ssl
+  client.port = port
 
-request = client.get(request) do |resp|
-  puts "got response #{resp.status_code}"
+  request = client.get(request) do |resp|
+    puts "got response #{resp.status_code}"
 
-  resp.body_handler do |body|
-    puts "The total body received was #{body.length} bytes"
-    puts body
-    puts "Exiting"
-    Vertx.exit
+    resp.body_handler do |body|
+      puts body
+    end
   end
+
+  request.put_header('X-Obsidian-Access-Key', access_key)
+  request.put_header("X-Obsidian-Timestamp", timestamp)
+  request.put_header('X-Obsidian-Signature', signature)
+  request.put_header('Accept', 'application/vnd.eventstore.atom+json')
+  request.put_header('Content-Length', body.length)
+  request.put_header('Content-Type', 'application/json')
+
+  request.exception_handler { |e| raise e }
+
+  request.write_str(body)
+
+  request.end
 end
-
-request.put_header('X-Obsidian-Access-Key', access_key)
-request.put_header("X-Obsidian-Timestamp", timestamp)
-request.put_header('X-Obsidian-Signature', signature)
-request.put_header('Accept', 'application/vnd.eventstore.atom+json')
-request.put_header('Content-Length', body.length)
-request.put_header('Content-Type', 'application/json')
-
-request.exception_handler { |e| raise e }
-
-request.write_str(body)
-
-request.end
